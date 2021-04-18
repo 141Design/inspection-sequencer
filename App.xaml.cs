31a3ca8ab09f87d918c97228c04372ce7e8d56b1
141Design <141.atlas@gmail.com>
2021-04-18 23:34:29 +0900

    add : 各種ファイルの追加
    MVVMモデルのベースとなるファイルを作成。

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,10 +8,27 @@ using System.Windows;
 
 namespace InspectionSequencer
 {
+    using InspectionSequencer.Views;
+    using InspectionSequencer.ViewModels;
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var inspView = new InspectionView();
+            var inspViewModel = new InspectionViewModel();
+            var errView = new ErrorView();
+            var errViewModel = new ErrorViewModel();
+
+            //ViewModelをデータコンテキストに指定
+            inspView.DataContext = inspViewModel;
+            errView.DataContext = errViewModel;
+
+            inspView.Show();
+
+        }
     }
 }
--- Views/InspectionView.xaml.cs
+++ Views/InspectionView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace InspectionSequencer
+namespace InspectionSequencer.Views
 {
     /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
+    /// InspectionView.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class InspectionView : Window
     {
-        public MainWindow()
+        public InspectionView()
         {
             InitializeComponent();
         }
